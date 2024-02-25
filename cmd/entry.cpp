fae42fcb65065607cfb18a7e99f356c528f0860d
DarkDumpTruck <131649948+DarkDumpTruck@users.noreply.github.com>
2024-02-25 12:52:04 +0000

    update entry

--- cmd/entry.cpp
+++ cmd/entry.cpp
@@ -3,7 +3,7 @@
 #include "games/OneTwoPoker/main.h"
 
 int main() {
-  printf("Choose game to start:\n\t1. GuessPlane\n\t2. MeetInMiddle\n");
+  printf("Choose game to start:\n\t1. GuessPlane\n\t2. MeetInMiddle\n\t3. OneTwoPoker\n");
   int x;
   scanf("%d", &x);
   if (x == 1) {
